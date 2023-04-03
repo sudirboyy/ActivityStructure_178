25f43d752a89c0466b6097509fabe0741ba6bd16
sudirboyy <sudirboyy@gmail.com>
2023-04-03 23:27:23 +0700

    Membuat cout dari struct mhs1 dan mhs2

--- ActivityStructure_178/ActivityStructure_178.cpp
+++ ActivityStructure_178/ActivityStructure_178.cpp
@@ -15,4 +15,26 @@ int main()
     mhs1.NIM = "20220140178";
     mhs1.nama = "Sudirman";
     mhs1.alamat = "Kulonprogo";
-    mhs1.umur = 20
+    mhs1.umur = 20;
+
+    cout << "Masukkan NIM : ";
+    cin >> mhs2.NIM;
+    cout << "Masukkan Nama : ";
+    cin >> mhs2.nama;
+    cout << "Masukkan Alamat : ";
+    cin >> mhs2.alamat;
+    cout << "Masukkan Umur : ";
+    cin >> mhs2.umur;
+
+    cout << "\nNIM : " << mhs1.NIM;
+    cout << "\nNama : " << mhs1.nama;
+    cout << "\nAlamat : " << mhs1.alamat;
+    cout << "\nUmmur : " << mhs1.umur << endl;
+
+    cout << "\nNIM : " << mhs2.NIM;
+    cout << "\nNama : " << mhs2.nama;
+    cout << "\nAlamat : " << mhs2.alamat;
+    cout << "\nUmur : " << mhs2.umur;
+
+
+}
