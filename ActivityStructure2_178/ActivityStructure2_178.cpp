366f3fee4f9f46135a12c5e67b0246ee6f151e7a
sudirboyy <sudirboyy@gmail.com>
2023-04-03 23:30:25 +0700

    Membuat COUT pada struct ke-2

--- ActivityStructure2_178/ActivityStructure2_178.cpp
+++ ActivityStructure2_178/ActivityStructure2_178.cpp
@@ -12,3 +12,28 @@ struct Mahasiswa {
     AlamatDetail alamat;
     int umur;
 };
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Mahasiswa mhs;
+
+    cout << "Masukkan NIM : ";
+    cin >> mhs.NIM;
+    cout << "Masukkan Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat: " << endl;
+    cout << "\tNama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\tNama Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Masukkan Umur : ";
+    cin >> mhs.umur;
+
+    cout << "\nNIM : " << mhs.NIM;
+    cout << "\nNama : " << mhs.nama;
+    cout << "\nDesa : " << mhs.alamat.desa;
+    cout << "\nKota : " << mhs.alamat.kota;
+    cout << "\nUmur : " << mhs.umur;
+
+}
