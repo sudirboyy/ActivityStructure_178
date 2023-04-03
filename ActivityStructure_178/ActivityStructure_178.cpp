a540f337470d01f7b43dc0182825d1677206b995
sudirboyy <sudirboyy@gmail.com>
2023-04-03 23:23:28 +0700

    Membuat pengisian struct mahasiswa.

--- ActivityStructure_178/ActivityStructure_178.cpp
+++ ActivityStructure_178/ActivityStructure_178.cpp
@@ -7,3 +7,12 @@ struct Mahasiswa {
     string alamat;
     int umur;
 };
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Mahasiswa mhs1, mhs2;
+    mhs1.NIM = "20220140178";
+    mhs1.nama = "Sudirman";
+    mhs1.alamat = "Kulonprogo";
+    mhs1.umur = 20
